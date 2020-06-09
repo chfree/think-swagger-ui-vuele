2c54834d93f78ff58a7984ff1aeca33fe6e3fea9
zfree <79763939@qq.com>
2020-06-09 12:55:02 +0800

    生成md完善

--- src/views/pages/swagger/assist/buildmd.js
+++ src/views/pages/swagger/assist/buildmd.js
@@ -10,12 +10,25 @@ export default {
       const reqMethod = this.menuInfo.reqMethod[this.activeName] || {}
       arrMds.push('# ' + reqMethod.summary)
       arrMds.push('## 请求头')
-      arrMds.push('### 请求地址')
-      arrMds.push('### 请求方式')
-      arrMds.push('### 响应Content-Type')
+      arrMds.push('| 名称 | 描述 |')
+      arrMds.push('| --- | --- |')
+      arrMds.push('| 请求地址 | ' + this.methodForm.requestPath + ' |')
+      arrMds.push('| 请求方式 | ' + this.activeName + ' |')
+      arrMds.push('| 响应Content-Type | ' + this.methodForm.contentType + ' |\r\n')
       arrMds.push('## 请求参数')
+      if (this.isPostJson) {
+        arrMds.push('```')
+        arrMds.push(this.parameters[0].value)
+        arrMds.push('```')
+      } else {
+        arrMds.push('| 名称 | 描述 | 是否必填 | 参数类型 | 数据类型 |')
+        arrMds.push('| --- | --- | --- | --- | --- |')
+        this.parameters.forEach(item => {
+          arrMds.push('| ' + item.name + ' | ' + (item.description || '') + ' | ' + item.required + ' | ' + item.in + ' | ' + item.type + ' |')
+        })
+      }
 
-      arrMds.push('## 响应参数')
+      arrMds.push('\r\n## 响应参数')
 
       return arrMds.join('\r\n')
     }
